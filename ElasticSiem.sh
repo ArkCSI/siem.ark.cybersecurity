e28bc374410057f497c1ee412e41910c0d7b64cb
Ark Cybersecurity Instituite <74528279+ArkCSI@users.noreply.github.com>
2023-05-19 02:16:11 -0600

    Update ElasticSiem.sh
    Se modifico el script para que el certificado ssl se generara correctamente

--- ElasticSiem.sh
+++ ElasticSiem.sh
@@ -16,7 +16,7 @@ curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch |  gpg --dearmor -
 echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" |  tee -a /etc/apt/sources.list.d/elastic-8.x.list
 
 # Instalando Elasticsearch
-bash -c "export HOSTNAME=siem.ark.cybersecurity; apt-get install elasticsearch -y | tee ~/ElasticLogs.md"
+bash -c "export HOSTNAME=siem.ark.cybersecurity; apt-get install elasticsearch -y | tee ElasticLogs.md"
 
 # Comentando una línea en el archivo de configuración de Elasticsearch
 sed -e '/cluster.initial_master_nodes/ s/^#*/#/' -i /etc/elasticsearch/elasticsearch.yml
@@ -35,7 +35,29 @@ echo "server.host: \"siem.ark.cybersecurity\"" |  tee -a /etc/kibana/kibana.yml
 
 # Habilitando y reiniciando los servicios de Elasticsearch y Kibana
 systemctl enable elasticsearch kibana --now
-
+echo " "
+echo "Por favor revisa que siem.ark.cybersecurity esté asociado a la  IP de tu Kali en /etc/hosts para permitir la conexión a Kibana"
+echo " "
+echo "Deberia verse similar a:"
+echo " "
+echo " 192.168.100.25   siem.ark.cybersecurity"
+# Generando token de enrolamiento y codigo de verificacion
+echo " "
+echo " Token:"
+/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana
+echo " "
+echo " Por favor visita http://{La IP de tu Kali}:5601"
+echo " Ej: http://192.168.100.25:5601"
+echo " "
+echo "En caso de que te lo solicite ingresa el usuario elastic y la contraseña que se genero despues de la instalacion"
+echo "En caso de que tomaras conta de ella puede hace un {cat ElasticLogs.md | grep superuser} en el directorio donde ejecutaste este script"
+read -p "Por favor, confirma que la acción se ha completado (presiona Enter para continuar)..."
+echo " "
+echo "Codigo de verificacion:"
+echo " "
+/usr/share/kibana/bin/kibana-verification-code
+echo " "
+read -p "Por favor, confirma que completaste la activacion de kibana (presiona Enter para continuar)..."
 # Generando un certificado de autoridad (CA) para Elasticsearch
 /usr/share/elasticsearch/bin/elasticsearch-certutil ca
 
@@ -63,18 +85,5 @@ echo "server.publicBaseUrl: \"https://siem.ark.cybersecurity:5601\"" |  tee -a /
 # Reiniciando el servicio  Kibana y Elasitcsearch
 systemctl restart kibana elasticsearch
 
-# Generando token de enrolamiento y codigo de verificacion
-echo " "
-echo "Por favor revisa que siem.ark.cybersecurity esté asociado a la  IP de tu Kali en /etc/hosts para permitir la conexión a Kibana"
-echo " "
-echo "Deberia ser algo similar a:"
-echo " "
-echo " 192.168.100.25   siem.ark.cybersecurity"
-echo " "
-echo " "
-echo " Token de enrolamiento:"
-/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana
-echo " "
-echo " Codigo de verificacion:"
-/usr/share/kibana/bin/kibana-verification-code
+
 
